f6dcd29bc8ff16b99d77df20a27be7bee105fa48
Richard Huaman <66971727+richardhaz@users.noreply.github.com>
2023-04-22 18:56:13 -0500

    separate api and socket to run in different ports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,20 +12,22 @@ import { chatSocket } from '@sockets/chat.socket';
 import { logger } from '@utils/logger';
 
 const appPort = envConfig().app.port as string;
+const socketPort = Number(envConfig().socket.port);
 const environment = envConfig().app.environment as string;
 
 const httpServer = http.createServer(server);
-
 const io = new SocketServer(httpServer, { cors: corsConfig });
 
 const bootstrap = async (): Promise<void> => {
   try {
-    logger.info('loading services..');
+    logger.info('Loading services..');
     const res = await dbConnect();
     if (res) {
-      logger.info('db connected');
+      logger.info('Db connected');
       chatSocket(io);
-      httpServer.listen(appPort, () => logger.info(`server running on ${`${environment}`} mode`));
+      server.listen(appPort, () => logger.info(`Server running on port ${appPort} in ${`${environment}`} mode`));
+      logger.info(`Socket running on port ${socketPort}`);
+      io.listen(socketPort);
     }
   } catch (error: any) {
     console.log(error.message);
