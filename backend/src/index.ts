54b6282ec6f0a74351f5e54db375af5bb2013d61
Richard Huaman <66971727+richardhaz@users.noreply.github.com>
2023-04-22 21:05:26 -0500

    revert socket config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,22 +12,20 @@ import { chatSocket } from '@sockets/chat.socket';
 import { logger } from '@utils/logger';
 
 const appPort = envConfig().app.port as string;
-const socketPort = Number(envConfig().socket.port);
 const environment = envConfig().app.environment as string;
 
 const httpServer = http.createServer(server);
+
 const io = new SocketServer(httpServer, { cors: corsConfig });
 
 const bootstrap = async (): Promise<void> => {
   try {
-    logger.info('Loading services..');
+    logger.info('loading services..');
     const res = await dbConnect();
     if (res) {
-      logger.info('Db connected');
+      logger.info('db connected');
       chatSocket(io);
-      server.listen(appPort, () => logger.info(`Server running on port ${appPort} in ${`${environment}`} mode`));
-      logger.info(`Socket running on port ${socketPort}`);
-      io.listen(socketPort);
+      httpServer.listen(appPort, () => logger.info(`server running on ${`${environment}`} mode`));
     }
   } catch (error: any) {
     console.log(error.message);
