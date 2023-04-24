2cd9d490825d9247f4ca4bc31c51fd150ea87ac9
Richard Huaman <66971727+richardhaz@users.noreply.github.com>
2023-04-23 23:15:39 -0500

    remove socket initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,21 @@
 import 'dotenv/config';
 
-import http from 'http';
-import { Server as SocketServer } from 'socket.io';
-
 import { envConfig } from '@config/env-config';
 import { dbConnect } from '@db/db-connection';
 
-import { corsConfig } from './config';
 import { server } from './server';
-import { chatSocket } from '@sockets/chat.socket';
 import { logger } from '@utils/logger';
 
 const appPort = envConfig().app.port as string;
 const environment = envConfig().app.environment as string;
 
-const httpServer = http.createServer(server);
-
-const io = new SocketServer(httpServer, { cors: corsConfig });
-
 const bootstrap = async (): Promise<void> => {
   try {
-    logger.info('loading services..');
+    logger.info('Loading services..');
     const res = await dbConnect();
     if (res) {
-      logger.info('db connected');
-      chatSocket(io);
-      httpServer.listen(appPort, () => logger.info(`server running on ${`${environment}`} mode`));
+      logger.info('Db connected');
+      server.listen(appPort, () => logger.info(`Server running on port ${appPort} in ${`${environment}`} mode`));
     }
   } catch (error: any) {
     console.log(error.message);
