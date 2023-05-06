49717bdad660d4e4648b4b423c3daf5381ea1d86
Richard Huaman <66971727+richardhaz@users.noreply.github.com>
2023-05-06 11:56:48 -0500

    add production path

--- backend/src/config/cors-config.ts
+++ backend/src/config/cors-config.ts
@@ -1,10 +1,12 @@
 import { envConfig } from './env-config';
 
 const defaultOrigins = ['http://localhost:5173', 'http://localhost:3005'];
-const prodOrigin = envConfig().app.allowedOrigin;
+const prodOrigins = envConfig().app.allowedOrigin?.split(',') as string[];
 
 export const corsConfig = {
   credentials: true,
   methods: 'GET, POST, PUT, PATCH, DELETE',
-  origin: prodOrigin ? [...defaultOrigins, prodOrigin] : defaultOrigins,
+  origin: prodOrigins ? [...defaultOrigins, ...prodOrigins] : defaultOrigins,
 };
+
+console.log(corsConfig.origin);
